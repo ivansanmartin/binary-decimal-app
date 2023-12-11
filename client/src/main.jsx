7351388f3271746612f808d7ae977b2b01dd1d10
Ivan San Martin <ivansanmartin987@gmail.com>
2023-12-11 00:32:20 -0300

    Reestructered code and added table for records

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/Root.jsx";
 import BinaryDecimal from "./routes/BinaryDecimal.jsx";
