8c7c3aa79bb86b818acc8fcfb2e67b631ddc14d0
Ivan San Martin <ivansanmartin987@gmail.com>
2024-01-22 01:58:13 -0300

    update: principal layout modified

--- client/src/routes/Index.jsx
+++ client/src/routes/Index.jsx
@@ -20,23 +20,58 @@ function Index() {
 
 
     return (
-        <header className="container-fluid d-flex justify-content-around mt-5">
-            <div className="logo">
-                <a href="/">Binary Decimal App</a>
-            </div>
+
+        <>
+            <header className="container-fluid d-flex justify-content-around mt-5">
+                <div className="logo">
+                    <a href="/">Binary Decimal App</a>
+                </div>
 
 
 
-            <div className="login-register">
-                {isAuthenticated ? (
-                    <a href="/dashboard">Dashboard</a>
-                ) : (
-                    <a href="" className="" onClick={() => loginWithRedirect()}>
-                        Iniciar sesión →
-                    </a>
-                )}
-            </div>
-        </header>
+                <div className="login-register">
+                    {isAuthenticated ? (
+                        <a href="/dashboard">Dashboard</a>
+                    ) : (
+                        <a href="" className="" onClick={() => loginWithRedirect()}>
+                            Iniciar sesión →
+                        </a>
+                    )}
+                </div>
+            </header>
+
+            <main>
+                <section className="app-information text-break">
+                    <div className="title">
+                        <div>
+                            <h1 className="fw-bold text-white">¡Descubre el mundo binario!</h1>
+                            <p className="text-white mt-4 fs-6">Convierte números y texto en emocionantes códigos binarios y desentraña sus secretos con nuestra increíble herramienta de transformación.</p>
+                        </div>
+
+                        <div className="btn-info">
+                            <button className="btn btn-primary fw-bold" onClick={() => loginWithRedirect()}>Comenzar</button>
+                            <a href="#learn-more" className="fw-bold">Leer más →</a>
+                        </div>
+                    </div>
+
+                    <div className="image">
+                        <img className="img-fluid" src="https://res.cloudinary.com/dxupqwg5l/image/upload/zohbwcqczr1irctekxmm.jpg" alt="" />
+                    </div>
+                </section>
+
+
+                <section className="learn-more" id="learn-more">
+                    <div className="info">
+                        <h1 className="fw-bold text-white">¿Cómo fue realizada esta aplicación?</h1>
+
+                    </div>
+
+                </section>
+
+            </main>
+        
+        </>
+
     );
 }
 
